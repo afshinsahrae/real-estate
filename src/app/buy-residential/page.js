54960f96a2431ee87c516f5cfb78125585516093
afshin.s <afshin.s220@gmail.com>
2026-01-01 02:32:39 +0330

    up2

--- src/app/buy-residential/page.js
+++ src/app/buy-residential/page.js
@@ -1,15 +1,18 @@
 ﻿import BuyResidentialsPage from "@/template/BuyResidentialsPage";
 
-async function BuyResidentials({searchParams}) {
-  const res = await fetch("/api/profile", {
+async function BuyResidentials({ searchParams }) {
+  const baseUrl = process.env.NEXTAUTH_URL || "http://localhost:3000";
+
+  const res = await fetch(`${baseUrl}/api/profile`, {
     cache: "no-store",
   });
+
   const data = await res.json();
 
-  let finalData=data.data
+  let finalData = data.data;
 
-  if(searchParams.category){
-    finalData=finalData.filter(i=>i.category===searchParams.category)
+  if (searchParams.category) {
+    finalData = finalData.filter((i) => i.category === searchParams.category);
   }
 
   if (data.error) return <h2>مشکلی پیش آمده است</h2>;
