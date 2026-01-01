602ccb3e18d5d76de66c9a93ad39fd6562d003ac
afshin.s <afshin.s220@gmail.com>
2026-01-01 18:48:23 +0330

    up3

--- src/app/buy-residential/page.js
+++ src/app/buy-residential/page.js
@@ -1,17 +1,50 @@
-﻿import BuyResidentialsPage from "@/template/BuyResidentialsPage";
+﻿// import BuyResidentialsPage from "@/template/BuyResidentialsPage";
 
-async function BuyResidentials({ searchParams }) {
-  const baseUrl = process.env.NEXTAUTH_URL || "http://localhost:3000";
+// async function BuyResidentials({ searchParams }) {
+//   const baseUrl = process.env.NEXTAUTH_URL || "http://localhost:3000";
 
-  const res = await fetch(`${baseUrl}/api/profile`, {
-    cache: "no-store",
-  });
+//   const res = await fetch(`${baseUrl}/api/profile`, {
+//     cache: "no-store",
+//   });
+
+//   const data = await res.json();
+
+//   let finalData = data.data;
+
+//   if (searchParams.category) {
+//     finalData = finalData.filter((i) => i.category === searchParams.category);
+//   }
+
+//   if (data.error) return <h2>مشکلی پیش آمده است</h2>;
+
+//   return <BuyResidentialsPage data={finalData} />;
+// }
+
+// export default BuyResidentials;
+/////////
+
+
+
+import BuyResidentialsPage from "@/template/BuyResidentialsPage";
+
+// اجبار به Dynamic Rendering
+export const dynamic = "force-dynamic";
+
+export default async function BuyResidentials({ searchParams }) {
+  const baseUrl = process.env.VERCEL_URL
+    ? `https://${process.env.VERCEL_URL}`
+    : "http://localhost:3000";
+
+  const res = await fetch(`${baseUrl}/api/profile`, { cache: "no-store" });
+
+  if (!res.ok) {
+    return <h2>مشکلی پیش آمده است</h2>;
+  }
 
   const data = await res.json();
-
   let finalData = data.data;
 
-  if (searchParams.category) {
+  if (searchParams?.category) {
     finalData = finalData.filter((i) => i.category === searchParams.category);
   }
 
@@ -20,4 +53,8 @@ async function BuyResidentials({ searchParams }) {
   return <BuyResidentialsPage data={finalData} />;
 }
 
-export default BuyResidentials;
+
+
+
+
+
